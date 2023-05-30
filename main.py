11b0c71de40af7827e7f3fdc0e1b4dc21f03bbd9
Achilles107 <abhinavshukla835@gmail.com>
2023-05-30 17:16:25 -0400

    Mask Model

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ st.title('PyTorch Style Transfer')
 
 img = st.sidebar.selectbox(
     'Select Image',
-    ('amber.jpg', 'cat.png')
+    ('girl_mask.png', 'selena.png')
 )
 
-style_name = st.sidebar.selectbox(
-    'Select Style',
-    ('candy', 'mosaic', 'rain_princess', 'udnie')
-)
+# style_name = st.sidebar.selectbox(
+#     'Select Style',
+#     ('candy', 'mosaic', 'rain_princess', 'udnie')
+# )
 
-
-model= "saved_models/" + style_name + ".pth"
+model = "mask_pred_weights.pth"
+#model= "saved_models/" + style_name + ".pth"
 input_image = "images/content-images/" + img
-output_image = "images/output-images/" + style_name + "-" + img
+#output_image = "images/output-images/" + style_name + "-" + img
 
 st.write('### Source image:')
 image = Image.open(input_image)
@@ -33,9 +33,7 @@ clicked = st.button('Stylize')
 
 if clicked:
     model = style.load_model(model)
-    style.stylize(model, input_image, output_image)
-
-    st.write('### Output image:')
-    image = Image.open(output_image)
-    st.image(image, width=400)
+    output = style.stylize(model, input_image)
+    print(output)
+    
 
--- style.py
+++ style.py
@@ -13,8 +13,8 @@ from torchvision import transforms
 import torch.onnx
 
 import utils
-from transformer_net import TransformerNet
-from vgg import Vgg16
+from transformer_net import MaskNoMaskClassifier
+import io
 import streamlit as st
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -23,7 +23,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def load_model(model_path):
     print('load model')
     with torch.no_grad():
-        style_model = TransformerNet()
+        style_model = MaskNoMaskClassifier()
         state_dict = torch.load(model_path)
         # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
         for k in list(state_dict.keys()):
@@ -35,16 +35,13 @@ def load_model(model_path):
         return style_model
 
 @st.cache
-def stylize(style_model, content_image, output_image):
+def stylize(style_model, content_image):
     content_image = utils.load_image(content_image)
-    content_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
-    ])
-    content_image = content_transform(content_image)
-    content_image = content_image.unsqueeze(0).to(device)
+    transformation = transforms.Compose([transforms.Lambda(lambda x: x.convert('RGB') if x.mode != 'RGB' else x),
+                                      transforms.Resize((150, 150)),  # resize to input shape of our CNN
+                                      transforms.ToTensor()  # convert PIL to Tensor
+                                      ])
+    content_image = transformation(content_image)
+    output = content_image.unsqueeze(0).to(device)
     
-    with torch.no_grad():
-        output = style_model(content_image).cpu()
-            
-    utils.save_image(output_image, output[0])
+    return output
